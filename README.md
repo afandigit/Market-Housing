95c2e8c9ae61f5f6f422d5e2d9c42048088bec37
AFANDI soufiane <afasou9@gmail.com>
2024-05-19 12:31:10 +0100

    market housing project - 2 scrapy spiders with selenium to scrap data from mubawab and avito websites

---
+++ README.md
@@ -0,0 +1,26 @@
+# Market Housing Project
+
+------------------------------------------------------------------------
+## Overview :
+
+I am using the Scrapy Framework to scrape datasets from websites offering real estate listings for sales and rentals. The goal is to analyze this data and develop a Machine Learning model that suggests listings to clients based on their preferences and financial capabilities.
+
+This involves extracting relevant features from the listings, such as location, price, property type, and amenities, and using these features to train a recommendation system.
+
+The system will leverage data analysis, data science techniques, and business intelligence to provide personalized and financially feasible real estate options for clients.
+
+This project aims to enhance the user experience and optimize the property search process, ultimately driving better business outcomes for real estate platforms.
+
+
+------------------------------------------------------------------------
+## Features :
+
+- Web Scraping : Scrapy Framework. [Current step ...]
+- Data Cleaning. [Current step ...]
+- Data Analysis - Explore and Visualize data : Python libraries - Microsoft Power BI. [Future step ...]
+- MERN Stack. [Future step ...]
+- Data Preparation for Machine Learning : Data standardization and preprocessing. [Future step ...]
+- Machine Learning. [Future step ...]
+- Deploy the trained recommendation model in real time. [Future step ...]
+
+
---
+++ housemarketing_scrapy/housemarketing_scrapy/pipelines.py
@@ -0,0 +1,71 @@
+from itemadapter import ItemAdapter
+import mysql.connector
+
+class HousemarketingScrapyPipeline:
+    def process_item(self, item, spider):
+        return item
+
+class SaveToMySqlPipeline:
+
+    def __init__(self):
+        self.conn = mysql.connector.connect(
+            host	  = 'localhost',
+            user 	  = 'root',
+            password  = 'root',
+            database  = 'mubawab'
+        )
+
+        self.cur = self.conn.cursor()
+        # self.cur.execute("""
+        #     DROP TABLE IF EXISTS house_advertisements
+        # """)
+        # CREATE TABLE IF NOT EXISTS house_advertisements_cleaned_data(
+        self.cur.execute("""
+            CREATE TABLE IF NOT EXISTS avito_house_advertisements_cleaned_data(
+            id int NOT NULL auto_increment,
+            advertisement_url VARCHAR(255),
+            title VARCHAR(255),
+            publication_date VARCHAR(255),
+            price VARCHAR(255),
+            location VARCHAR(255),
+            description TEXT,
+            complete_description TEXT,
+            features_list VARCHAR(255),
+            insert_date DATE DEFAULT (CURRENT_DATE),
+            PRIMARY KEY (id)
+            )
+        """)
+
+    def process_item(self, item, spider):
+
+        ## Define insert statement
+        self.cur.execute(""" insert into avito_house_advertisements_cleaned_data (
+            advertisement_url,
+            title,
+            publication_date,
+            price,
+            location,
+            description,
+            complete_description,
+            features_list
+            ) values ( %s, %s, %s, %s, %s, %s, %s, %s )  
+            """ , (
+                item["advertisement_url"],
+                item["title"],
+                item["publication_date"],
+                item["price"],
+                item["location"],
+                item["description"],
+                item["complete_description"],
+                item["features_list"]
+            ))
+        
+        ## Execute insert of data into database
+        self.conn.commit()
+        return item
+
+    def close_spider(self, spider):
+
+        ## Close cursor & connection to database
+        self.cur.close()
+        self.conn.close()
